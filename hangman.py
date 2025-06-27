88813d7bf390345cf9f5cc8997b2648c0469e03e
shellockops <218080613+shellockops@users.noreply.github.com>
2025-06-28 00:12:29 +0200

    Consistent use of indentation

--- hangman.py
+++ hangman.py
@@ -1,9 +1,9 @@
 import random
 
 def update_word(character, word, word_to_guess):
-	for i in range(len(word)):
-		if word[i] == character:
-			word_to_guess[i] = character
+  for i in range(len(word)):
+    if word[i] == character:
+      word_to_guess[i] = character
 
 def clear_screen():
 	print("\033[2J\033[H")
